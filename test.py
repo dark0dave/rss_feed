a7d589f56b68bbd617effaa09cf8ec8aab7246e4
dark0dave <dark0dave@mykolab.com>
2019-12-31 05:47:35 +0000

    Added better tests
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- test.py
+++ test.py
@@ -1,11 +1,22 @@
-import opml
+import asyncio
+from opml import parse
+from requests import get
 
-def main():
-    noDuplicates(opml.parse('backup.opml'))
+async def main():
+    urls = [opml._root.get('xmlUrl') for opml in parse('backup.opml')]
+    noDuplicates(urls)
+    [
+        await checkUrlExists(url) 
+        for url in urls 
+        if 'reddit' not in url and 'edri' not in url
+    ]
 
-def noDuplicates(opmls):
-    urls = [x._root.get('xmlUrl') for x in opmls]
+def noDuplicates(urls):
     assert len(urls) == len(set(urls))
 
+async def checkUrlExists(url):
+    status = get(url).status_code
+    assert 200 == status
+
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
