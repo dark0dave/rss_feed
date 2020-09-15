ed980e10280854ac130ebe86d7d1d2aa5b43ebe0
dark0dave <dark0dave@mykolab.com>
2020-09-15 23:54:17 +0100

    Updated to newest version also removed Pipfile, as it does not support newer versions of python
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-import asyncio
 from opml import parse
 from requests import get
 
-async def main():
+def main():
     urls = [opml._root.get('xmlUrl') for opml in parse('backup.opml')]
     noDuplicates(urls)
     [
-        await checkUrlExists(url) 
+        checkUrlExists(url) 
         for url in urls 
         if 'reddit' not in url and 'edri' not in url
     ]
@@ -14,9 +13,10 @@ async def main():
 def noDuplicates(urls):
     assert len(urls) == len(set(urls))
 
-async def checkUrlExists(url):
+def checkUrlExists(url):
     status = get(url).status_code
+    print(url)
     assert 200 == status
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
